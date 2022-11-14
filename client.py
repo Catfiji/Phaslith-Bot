c13149f52e4255096a23fbdb32b4a8c1638968f5
KawaiICat <Cathoniaherz@gmail.com>
2022-11-14 09:02:10 -0800

    Added hi :)

--- client.py
+++ client.py
@@ -5,7 +5,7 @@ from discord.ext import commands
 
 client = commands.Bot(command_prefix=config.PREFIX, intents=discord.Intents.all(), help_command=None)
 #;---------------------------------------------------------------------------
-
+# hi
 async def load_cogs():
     cogs_loaded = 0 # Slice of Life
     cogs = ["Cogs.Utilities.Utilities", "Cogs.Admin.Admin", "Cogs.User.User", "Cogs.Currency.Currency"]
